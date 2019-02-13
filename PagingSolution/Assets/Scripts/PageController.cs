8f5e064225fe3e9a2b7e2ce5ec1801aae7518fbb
sergio-nnz <sergio.nnsa@gmail.com>
2019-02-12 23:19:03 -0800

    Added the initial paging scripts with some comments

---
+++ PagingSolution/Assets/Scripts/PageController.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PageController : MonoBehaviour
+{
+    [SerializeField] private Button _nextButton;
+    [SerializeField] private Button _previousButton;
+    [SerializeField] private TextMeshProUGUI _textPage;
+
+    // IPage pageSelection = new PageSelector();
+
+    public void SetTextForPaging()
+    {
+        //  Sets new text to the pager
+    }
+
+    public void NexPage()
+    {
+        // Set text from a certain iterator?
+    }
+
+    public void PreviousPage()
+    {
+        // Set text from a certain iterator
+    }
+
+    public void ToggleButtons()
+    {
+        // Turns buttons on and off
+    }
+}
